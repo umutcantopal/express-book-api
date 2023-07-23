49cdb27537bc904eb02efa77c721d18a010af85f
umut <umut.cantopal.199@gmail.com>
2023-07-23 22:06:51 +0300

    custom error handler extra conditions

--- middleware/error.js
+++ middleware/error.js
@@ -1,12 +1,22 @@
 import ErrorResponse from "../utils/ErrorResponse.js"
 
 const errorHandler = (err, req, res, next) => {
-    console.log(err.name)
+    // console.log(err)
     let error = { ...err }
     error.message = err.message
-    if (err.name === 'CastError') {
-        const message = `Content not found`
+    if (err.name === 'CastError') { // bad request id error
+        const message = 'Content not found'
         error = new ErrorResponse(message, 404)
+    }
+
+    if (err.code === 11000) { // duplicate entity error
+        const message = 'Duplicate Content'
+        error = new ErrorResponse(message, 400)
+    }
+
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(el => el.message)
+        error = new ErrorResponse(message, 400)
     }
 
     res.status(error.statusCode || 500).json({
